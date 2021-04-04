3d1621665648a6368026748a605ee1cb9b0c47c4
Robert Hammond <hamrobe502@gmail.com>
2021-04-04 03:05:49 -0500

    modified algorithm to use weights

--- script.py
+++ script.py
@@ -1,23 +1,17 @@
+import numpy as np
 from spotipy.client import Spotify
 import lastfm
 import pylast
 import pandas as pd
 import spotify
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.decomposition import PCA 
 import random
 
 # number of tracks from each user
-N = 32
-
-def get_shared_playlist(tracks1, tracks2):
-    # create a dataframe for each user
-    df1 = pd.DataFrame()
-    df2 = pd.DataFrame()
-
-    # collect info from track
-    return []
+NUM_SONGS_FROM_USERS = 50
+NUM_CLUSTERS = 20
     
 def get_df(tracks):
     weights = []
@@ -56,42 +50,28 @@ def get_df(tracks):
         'duration_ms': durations,
         'explicit': explicits,
         'album': albums,
-        'popularity': popularities
+        'popularity': popularities,
+        'weight': weights
     })
     return df
 
-def get_shared_playlist(name1, name2):
-    network = lastfm.get_network(name1)
-    # use network to retrive User objects
-    usr1 = network.get_user(name1)
-    usr2 = network.get_user(name2)
-
-    # collect the top tracks form 
-    tracks1 = usr1.get_top_tracks(limit = N, period=pylast.PERIOD_OVERALL)
-    tracks2 = usr2.get_top_tracks(limit = N, period=pylast.PERIOD_OVERALL)
-    
-    df1 = get_df(tracks1)
-    df2 = get_df(tracks2)
-    all_songs = pd.concat([df1, df2])
-    all_songs.reset_index(drop=True, inplace=True)
-
-    dfs = spotify.add_features(df1, df2)
-
-    
+# Create list of lists of song ids to put into recommendation function
+# this is the "main algorithm" where all the data science happens
+def get_list_of_recs(dfs, all_songs):
     columns = ['danceability','energy','speechiness','acousticness','valence','tempo']
     scaler = MinMaxScaler()
     scaler.fit(dfs[columns])
     dfs[columns] = scaler.transform(dfs[columns])
+    scaler = StandardScaler()
+    scaler.fit(dfs[['weight']])
+    dfs[['weight']] = scaler.transform(dfs[['weight']])
+    dfs[['weight']] -= np.min(dfs[['weight']]) - 0.4
 
-    clusters = 10
-    kmeans = KMeans(n_clusters=clusters)
+    kmeans = KMeans(n_clusters=NUM_CLUSTERS)
     kmeans.fit(dfs)
-    pca = PCA(3) 
-    pca.fit(dfs)   
-    pca_data = pd.DataFrame(pca.transform(dfs))
-    scaler = MinMaxScaler()
-    scaled = scaler.fit_transform(dfs)
-    y_kmeans = kmeans.fit_predict(scaled)
+    # scaler = MinMaxScaler()
+    # scaled = scaler.fit_transform(dfs)
+    y_kmeans = kmeans.fit_predict(dfs)
     dfs['cluster'] = y_kmeans
     dfs['artist'] = all_songs.artist.tolist()
     dfs['title'] = all_songs.song.tolist()
@@ -100,7 +80,7 @@ def get_shared_playlist(name1, name2):
     delete_clusters = []
     cluster = 0
     while cluster < (len(dfs.cluster.unique())-1):
-        if dfs.groupby('cluster').count().loc[cluster].danceability == 1:
+        if dfs.groupby('cluster').count().loc[cluster].danceability <= 1:
             delete_clusters.append(cluster)
         cluster += 1
     dfs.reset_index(inplace=True)
@@ -108,43 +88,80 @@ def get_shared_playlist(name1, name2):
     while i < (len(dfs.cluster.unique())-1):
         if dfs.loc[[i]].cluster.tolist()[0] in delete_clusters:
             dfs.drop(i,0,inplace=True)
-        i+=1
+        i += 1
     dfs.set_index('id',inplace=True)
 
-    # Create list of lists of song ids to put into recommendation function
+    # Create list of lists of song ids
     i=0
-    list_of_recs = [0]*len(dfs.groupby('cluster').count())
+    recs = [0]*len(dfs.groupby('cluster').count())
     while i<len(dfs.groupby('cluster').count()):
-        list_of_recs[i] = dfs.loc[dfs['cluster'] == i].index.to_list()
+        recs[i] = dfs.loc[dfs['cluster'] == i].index.to_list()
         i+=1
 
-    list_of_recs = [ele for ele in list_of_recs if ele != []] 
-    len(list_of_recs)
+    recs = [ele for ele in recs if ele != []] 
 
     # Adjust list for clusters so that each cluster has a maximum of 5 seed songs
-    j = 0
-    adj_list_of_recs = [0]*len(list_of_recs)
-    while j<len(list_of_recs):
-        if 0 < len(list_of_recs[j]) < 6:
-            adj_list_of_recs[j] = list_of_recs[j]
-        elif len(list_of_recs[j]) > 5:
-            adj_list_of_recs[j] = random.sample(list_of_recs[j], 5)
-        j += 1
+    for i in range(len(recs)):
+        if len(recs[i]) > 5:
+            recs[i] = random.sample(recs[i], 5)
+    
+    rec_weights = []
+    for cluster in recs:
+        total_weight = 0
+        for song in cluster:
+            info = dfs.loc[song]
+            w = info.loc['weight']
+            total_weight += w
+        rec_weights.append(total_weight)
 
-    len(adj_list_of_recs)
+    return [(recs[i], rec_weights[i]) for i in range(len(recs))]
 
-    list_of_recommendations = spotify.get_recs(list_of_recs, adj_list_of_recs)
-    list_of_recommendations_converted = [0]*len(list_of_recs)
 
-    l = 0
-    while l < len(list_of_recs):
-        list_of_recommendations_converted.append(pd.json_normalize(list_of_recommendations[l], record_path='tracks').id.tolist())
-        l += 1
+def get_shared_playlist(name1, name2):
+    network = lastfm.get_network(name1)
+    # use network to retrive User objects
+    usr1 = network.get_user(name1)
+    usr2 = network.get_user(name2)
 
-    no_integers = [x for x in list_of_recommendations_converted if not isinstance(x, int)]
-    list_of_recommendations_converted = [item for elem in no_integers for item in elem]
+    # collect the top tracks form 
+    tracks1 = []
+    tracks2 = []
+    try:
+        tracks1 = usr1.get_top_tracks(limit = NUM_SONGS_FROM_USERS, period=pylast.PERIOD_OVERALL)
+    except:
+        tracks1 = []
+    if not tracks1:
+        return f"Error: Could not find user \"{name1}\" in last.fm", True
+    try:
+        tracks2 = usr2.get_top_tracks(limit = NUM_SONGS_FROM_USERS, period=pylast.PERIOD_OVERALL)
+    except:
+        tracks2 = []
+    if not tracks2:
+        return f"Error: Could not find user \"{name2}\" in last.fm", True
+    
+    df1 = get_df(tracks1)
+    df2 = get_df(tracks2)
+    # normalize weights to an average of 1
+    df1.weight *= 1 / np.mean(df1.weight)
+    df2.weight *= 1 / np.mean(df2.weight)
+    all_songs = pd.concat([df1, df2])
+    all_songs.reset_index(drop=True, inplace=True)
 
-    return list_of_recommendations_converted
+    dfs = spotify.add_features(df1, df2)
+    # all_songs['id'] = dfs.loc['id']
+    
+    recs = get_list_of_recs(dfs, all_songs)
+
+    recommendations, num_recs = spotify.get_recs(recs, 30)
+    recommendations_converted = [
+        pd.json_normalize(rec, record_path='tracks').id.tolist()
+        for rec in recommendations
+    ]
+
+    no_integers = [x for x in recommendations_converted if not isinstance(x, int)]
+    recommendations_converted = [item for elem in no_integers for item in elem]
+
+    return recommendations_converted, False
 
 # for testing purposes
 if __name__  == "__main__":
